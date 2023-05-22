ef7fb8bdc55151b3c127d36b4d06ba7f8c3e1d20
Hoang Luong <holuong@expedia.com>
2023-05-22 21:03:50 +0900

    Init

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Hoang Luong on 22/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
